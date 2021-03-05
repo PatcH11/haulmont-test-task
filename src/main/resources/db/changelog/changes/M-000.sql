f1ce3e71099d4660ea52063a8c9a9046a651c67c
nikolay.evsyukov <patch.17@yandex.ru>
2021-03-05 21:23:29 +0300

    Дописал скрипт для создания бд

--- src/main/resources/db/changelog/changes/M-000.sql
+++ src/main/resources/db/changelog/changes/M-000.sql
@@ -9,3 +9,50 @@ CREATE TABLE clients
     passport_number INT          NOT NULL,
     PRIMARY KEY (client_id)
 );
+
+
+
+CREATE TABLE credits
+(
+    credit_id     VARCHAR(36) NOT NULL,
+    loan_limit    INT         NOT NULL,
+    interest_rate DOUBLE      NOT NULL,
+    PRIMARY KEY (credit_id),
+);
+
+
+
+CREATE TABLE banks
+(
+    client_id VARCHAR(36) NOT NULL,
+    credit_id VARCHAR(36) NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES clients (client_id),
+    FOREIGN KEY (credit_id) REFERENCES credits (credit_id)
+);
+
+
+
+CREATE TABLE credit_offers
+(
+    credit_offer_id VARCHAR(36) NOT NULL,
+    client_id       VARCHAR(36) NOT NULL,
+    credit_id       VARCHAR(36) NOT NULL,
+    credit_amount   DOUBLE      NOT NULL,
+    PRIMARY KEY (credit_offer_id),
+    FOREIGN KEY (client_id) REFERENCES clients (client_id),
+    FOREIGN KEY (credit_id) REFERENCES credits (credit_id)
+);
+
+
+
+CREATE TABLE payment_schedules
+(
+    payment_schedule_id          VARCHAR(36) NOT NULL,
+    credit_offer_id              VARCHAR(36) NOT NULL,
+    date                         TIMESTAMP   NOT NULL,
+    amount_payment               DOUBLE      NOT NULL,
+    repayment_amount_loan_body   DOUBLE      NOT NULL,
+    repayment_amount_percentages DOUBLE      NOT NULL,
+    PRIMARY KEY (payment_schedule_id),
+    FOREIGN KEY (credit_offer_id) REFERENCES credit_offers (credit_offer_id)
+);
