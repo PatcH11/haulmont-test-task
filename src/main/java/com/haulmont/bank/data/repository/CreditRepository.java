a87011dbf0c11664d313d553aecdeb085ced8539
patch11 <patch.17@yandex.ru>
2021-03-13 01:52:44 +0300

    Добавил новый метод в репозиторий

--- src/main/java/com/haulmont/bank/data/repository/CreditRepository.java
+++ src/main/java/com/haulmont/bank/data/repository/CreditRepository.java
@@ -1,9 +1,13 @@
 package com.haulmont.bank.data.repository;
 
+import com.haulmont.bank.data.model.Client;
 import com.haulmont.bank.data.model.Credit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CreditRepository extends JpaRepository<Credit, UUID> {
+
+    List<Credit> findCreditByClientsIs(Client client);
 }
