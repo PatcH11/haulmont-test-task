6be72d9310e31ff39e08dd2c5aba97fdb0b039dd
patch11 <patch.17@yandex.ru>
2021-03-13 01:51:53 +0300

    Добавил новые методы в репозитории

--- src/main/java/com/haulmont/bank/data/repository/CreditOfferRepository.java
+++ src/main/java/com/haulmont/bank/data/repository/CreditOfferRepository.java
@@ -1,9 +1,16 @@
 package com.haulmont.bank.data.repository;
 
+import com.haulmont.bank.data.model.Client;
+import com.haulmont.bank.data.model.Credit;
 import com.haulmont.bank.data.model.CreditOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CreditOfferRepository extends JpaRepository<CreditOffer, UUID> {
+
+    List<CreditOffer> findAllByClientIs(Client client);
+
+    CreditOffer findByClientIsAndCreditIs(Client client, Credit credit);
 }
--- src/main/java/com/haulmont/bank/data/repository/PaymentScheduleRepository.java
+++ src/main/java/com/haulmont/bank/data/repository/PaymentScheduleRepository.java
@@ -4,9 +4,12 @@ import com.haulmont.bank.data.model.CreditOffer;
 import com.haulmont.bank.data.model.PaymentSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentScheduleRepository extends JpaRepository<PaymentSchedule, UUID> {
 
     PaymentSchedule findFirstByCreditOfferOrderByDateDesc(CreditOffer creditOffer);
+
+    List<PaymentSchedule> findByCreditOfferOrderByDate(CreditOffer creditOffer);
 }
