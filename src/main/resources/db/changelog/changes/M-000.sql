ead67555f3da876080a9f99e3a714f3c43b55f56
patch11 <patch.17@yandex.ru>
2021-03-15 02:02:01 +0300

    Изменил скрипт для создания бд

--- src/main/resources/db/changelog/changes/M-000.sql
+++ src/main/resources/db/changelog/changes/M-000.sql
@@ -4,9 +4,9 @@ CREATE TABLE clients
     first_name      VARCHAR(100) NOT NULL,
     last_name       VARCHAR(100) NOT NULL,
     patronymic      VARCHAR(100) NOT NULL,
-    phone_number    VARCHAR(15)  NOT NULL,
-    email           VARCHAR(100) NOT NULL,
-    passport_number INT          NOT NULL,
+    phone_number    VARCHAR(15)  UNIQUE NOT NULL,
+    email           VARCHAR(100) UNIQUE NOT NULL,
+    passport_number INT          UNIQUE NOT NULL,
     PRIMARY KEY (client_id)
 );
 
@@ -15,7 +15,7 @@ CREATE TABLE clients
 CREATE TABLE credits
 (
     credit_id     UUID         NOT NULL,
-    name          VARCHAR(100) NOT NULL,
+    name          VARCHAR(100) UNIQUE NOT NULL,
     loan_limit    INT          NOT NULL,
     interest_rate DOUBLE       NOT NULL,
     PRIMARY KEY (credit_id)
@@ -28,8 +28,8 @@ CREATE TABLE bank
     client_id UUID NOT NULL,
     credit_id UUID NOT NULL,
     PRIMARY KEY (client_id, credit_id),
-    FOREIGN KEY (client_id) REFERENCES clients (client_id),
-    FOREIGN KEY (credit_id) REFERENCES credits (credit_id)
+    FOREIGN KEY (client_id) REFERENCES clients (client_id) ON DELETE CASCADE,
+    FOREIGN KEY (credit_id) REFERENCES credits (credit_id) ON DELETE CASCADE
 );
 
 
@@ -41,8 +41,8 @@ CREATE TABLE credit_offers
     credit_id       UUID   NOT NULL,
     credit_amount   DOUBLE NOT NULL,
     PRIMARY KEY (credit_offer_id),
-    FOREIGN KEY (client_id) REFERENCES clients (client_id),
-    FOREIGN KEY (credit_id) REFERENCES credits (credit_id)
+    FOREIGN KEY (client_id) REFERENCES clients (client_id) ON DELETE CASCADE,
+    FOREIGN KEY (credit_id) REFERENCES credits (credit_id) ON DELETE CASCADE
 );
 
 
@@ -57,5 +57,5 @@ CREATE TABLE payment_schedules
     repayment_amount_percentages DOUBLE                              NOT NULL,
     indebtedness                 DOUBLE                              NOT NULL,
     PRIMARY KEY (payment_schedule_id),
-    FOREIGN KEY (credit_offer_id) REFERENCES credit_offers (credit_offer_id)
+    FOREIGN KEY (credit_offer_id) REFERENCES credit_offers (credit_offer_id) ON DELETE CASCADE
 );
