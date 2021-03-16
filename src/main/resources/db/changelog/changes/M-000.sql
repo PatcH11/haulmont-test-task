5295e6307a0c63dc68404328034c889ed88f70c0
patch11 <patch.17@yandex.ru>
2021-03-16 13:12:35 +0300

    Изменил скрипт для создания бд

--- src/main/resources/db/changelog/changes/M-000.sql
+++ src/main/resources/db/changelog/changes/M-000.sql
@@ -1,12 +1,12 @@
 CREATE TABLE clients
 (
-    client_id       UUID         NOT NULL,
-    first_name      VARCHAR(100) NOT NULL,
-    last_name       VARCHAR(100) NOT NULL,
-    patronymic      VARCHAR(100) NOT NULL,
-    phone_number    VARCHAR(15)  UNIQUE NOT NULL,
+    client_id       UUID                NOT NULL,
+    first_name      VARCHAR(100)        NOT NULL,
+    last_name       VARCHAR(100)        NOT NULL,
+    patronymic      VARCHAR(100)        NOT NULL,
+    phone_number    VARCHAR(15) UNIQUE  NOT NULL,
     email           VARCHAR(100) UNIQUE NOT NULL,
-    passport_number INT          UNIQUE NOT NULL,
+    passport_number INT UNIQUE          NOT NULL,
     PRIMARY KEY (client_id)
 );
 
@@ -14,10 +14,10 @@ CREATE TABLE clients
 
 CREATE TABLE credits
 (
-    credit_id     UUID         NOT NULL,
+    credit_id     UUID                NOT NULL,
     name          VARCHAR(100) UNIQUE NOT NULL,
-    loan_limit    INT          NOT NULL,
-    interest_rate DOUBLE       NOT NULL,
+    loan_limit    INT                 NOT NULL,
+    interest_rate DOUBLE              NOT NULL,
     PRIMARY KEY (credit_id)
 );
 
@@ -28,8 +28,8 @@ CREATE TABLE bank
     client_id UUID NOT NULL,
     credit_id UUID NOT NULL,
     PRIMARY KEY (client_id, credit_id),
-    FOREIGN KEY (client_id) REFERENCES clients (client_id) ON DELETE CASCADE,
-    FOREIGN KEY (credit_id) REFERENCES credits (credit_id) ON DELETE CASCADE
+    FOREIGN KEY (client_id) REFERENCES clients (client_id),
+    FOREIGN KEY (credit_id) REFERENCES credits (credit_id)
 );
 
 
